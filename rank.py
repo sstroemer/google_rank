a79be3fd22667a241002825780b263893ec05e28
Stefan Strömer <s.stroemer@live.at>
2018-04-17 10:32:45 +0200

    changed to strange python3 format under windows

--- rank.py
+++ rank.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 import sys
-import urllib2
+#import urllib2
 import urllib
+from urllib.request import urlopen, Request
+import urllib.parse
 
 def main(argv):
 	if (len(argv) != 2):
-		print "Please specify domain name!"
+		print("Please specify domain name!")
 		return 1
 	
 	domain = argv[1]
@@ -13,23 +15,23 @@ def main(argv):
 
 	for i in range(0, len(keywords)-1):
 		keywords[i] = keywords[i].strip("\n")
-		print keywords[i] + " -> " + str(parseResults(search(keywords[i]), domain))
+		print(keywords[i] + " -> " + str(parseResults(search(keywords[i]), domain)))
 
 def parseResults(result, domain):
 	start = 0
 	rank = 1
 	while (True):
-		start = result.find('<h3 class="r"><a href', start)
+		start = result.find(b'<h3 class="r"><a href', start)
 		if (start == -1):
 			break
 
 		start += 23
-		end   = result.find('"', start)
+		end   = result.find(b'"', start)
 
-		# print result[start:end]
+		# print(result[start:end])
 
-		if (result[start:end].find(domain) != -1):
-			# print "Domain hit. Rank: " + rank
+		if (result[start:end].find(domain.encode()) != -1):
+			# print("Domain hit. Rank: " + rank)
 			break
 
 		rank += 1	
@@ -43,11 +45,11 @@ def readKeywords():
 	return lines
 
 def getHTML(url):
-	req = urllib2.Request(url, headers={ 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'})
-	return urllib2.urlopen(req).read()
+	req = Request(url, headers={ 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'})
+	return urlopen(req).read()
 
 def search(keyword):
-	return getHTML("http://www.google.at/search?num=100&q=" + urllib.quote_plus(keyword))
+	return getHTML("http://www.google.at/search?num=100&q=" + urllib.parse.quote_plus(keyword))
 
 if __name__ == "__main__":
     main(sys.argv)
