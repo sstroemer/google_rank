fb19ee10678ba675b65fabed7d0bbce42a4a60df
Stefan Strömer <s.stroemer@live.at>
2018-04-19 15:29:59 +0200

    csv

--- rank.py
+++ rank.py
@@ -1,55 +1,79 @@
 #!/usr/bin/python
 import sys
-#import urllib2
 import urllib
-from urllib.request import urlopen, Request
-import urllib.parse
+import datetime
+import time
 
-def main(argv):
-	if (len(argv) != 2):
-		print("Please specify domain name!")
-		return 1
-	
-	domain = argv[1]
+from urllib import request, parse
+
+
+def main():
+	domains = readDomains()
 	keywords = readKeywords()
 
-	for i in range(0, len(keywords)-1):
-		keywords[i] = keywords[i].strip("\n")
-		print(keywords[i] + " -> " + str(parseResults(search(keywords[i]), domain)))
+	for keyword in keywords:
+		print('-----------------------------------------')
+		print('Checking "' + keyword + '"')
+		parseResults(search(keyword), keyword, domains)
+		print('Delay')
+		time.sleep(60)
 
-def parseResults(result, domain):
+		
+def parseResults(result, keyword, domains):
 	start = 0
 	rank = 1
-	while (True):
+	
+	while True:
 		start = result.find(b'<h3 class="r"><a href', start)
-		if (start == -1):
-			break
+		if start == -1:
+			# write result to file (100 means > 100)
+			file = open('results\\' + keyword + '.csv', 'a+')
+			file.write('%s;%d;%s\n' % (datetime.datetime.now(), 100, domain))
+			file.close()
+			return
 
 		start += 23
 		end   = result.find(b'"', start)
 
-		# print(result[start:end])
-
-		if (result[start:end].find(domain.encode()) != -1):
-			# print("Domain hit. Rank: " + rank)
-			break
+		for domain in domains:
+			if result[start:end].find(domain.encode()) != -1:
+				# output
+				print('Domain ' + domain + ' hit. Rank: ' + str(rank))
+				# write result to file
+				file = open('results\\' + keyword + '.csv', 'a+')
+				file.write('%s;%d;%s\n' % (datetime.datetime.now(), rank, domain))
+				file.close()
+				return
 
 		rank += 1	
-	return rank
 
 
 def readKeywords():
 	file = open("keywords.txt", "r")
 	lines = file.readlines()
 	file.close()
+
+	lines = list(map(str.strip, lines))
 	return lines
+	
+	
+def readDomains():
+	file = open("domains.txt", "r")
+	lines = file.readlines()
+	file.close()
 
+	lines = list(map(str.strip, lines))
+	return lines	
+
+	
 def getHTML(url):
-	req = Request(url, headers={ 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'})
-	return urlopen(req).read()
+	req = urllib.request.Request(url, headers={ 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36' })
+	return urllib.request.urlopen(req).read()
 
+	
 def search(keyword):
 	return getHTML("http://www.google.at/search?num=100&q=" + urllib.parse.quote_plus(keyword))
 
+	
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
